2a4238baec08d80f5f2fa34306915083bfee820d
HaJaeEui <ha3310@naver.com>
2022-03-08 19:31:46 +0900

    220308하재의

--- CollaB/src/main/java/co/Donggle/CollaB/memberPage/service/MemWorkspaceJoinService.java
+++ CollaB/src/main/java/co/Donggle/CollaB/memberPage/service/MemWorkspaceJoinService.java
@@ -6,5 +6,5 @@ import co.Donggle.CollaB.workspace.service.WorkspaceJoinVO;
 
 public interface MemWorkspaceJoinService {
 	List<WorkspaceJoinVO> workspaceJoinIdselect(WorkspaceJoinVO vo); // 한 워크스페이스에 있는 멤버리스트
-	
+	WorkspaceJoinVO workspaceJoinUserIdselect(WorkspaceJoinVO vo);
 }
--- CollaB/src/main/java/co/Donggle/CollaB/memberPage/web/MemberPageController.java
+++ CollaB/src/main/java/co/Donggle/CollaB/memberPage/web/MemberPageController.java
@@ -1,6 +1,7 @@
 package co.Donggle.CollaB.memberPage.web;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import javax.servlet.http.HttpSession;
@@ -9,8 +10,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import co.Donggle.CollaB.board.service.BoardService;
+import co.Donggle.CollaB.board.service.BoardVO;
 import co.Donggle.CollaB.memberPage.service.MemWorkspaceJoinService;
+import co.Donggle.CollaB.user.service.UserVO;
 import co.Donggle.CollaB.workspace.service.WorkspaceJoinService;
 import co.Donggle.CollaB.workspace.service.WorkspaceJoinVO;
 import co.Donggle.CollaB.workspace.service.WorkspaceService;
@@ -28,6 +33,8 @@ public class MemberPageController {
 	@Autowired
 	MemWorkspaceJoinService memWorkspaceJoinDao;
 	
+	@Autowired
+	BoardService boardDao;
 	
 	@RequestMapping("/memberPage.do")
 	public String memberPage(Model model, HttpSession session, WorkspaceVO vo, WorkspaceJoinVO jvo) {
@@ -35,16 +42,50 @@ public class MemberPageController {
 		vo.setWorkspace_id(1);
 		jvo.setWorkspace_id(1);
 		
-		List<WorkspaceJoinVO> list = new ArrayList<WorkspaceJoinVO>();
-		list = memWorkspaceJoinDao.workspaceJoinIdselect(jvo);
-		System.out.println("================="+list);
 		model.addAttribute("workspace", workspaceDao.searchWorkspace(vo));
 		model.addAttribute("workspaceList", workspaceJoinDao.workspaceJoinList(userId)); // 사용자가 가지고 있는 모든 워크스페이스
-		model.addAttribute("user_infoList", workspaceJoinDao.workspaceTotalMember(jvo)); // 유저들의 총 정보
-		model.addAttribute("workspaceUserList", memWorkspaceJoinDao.workspaceJoinIdselect(jvo));
+//		model.addAttribute("user_infoList", workspaceJoinDao.workspaceTotalMember(jvo)); // 워크스페이스의 유저 총 수
+//		model.addAttribute("workspaceUserList", memWorkspaceJoinDao.workspaceJoinIdselect(jvo)); // 워크스페이스 권한
+		
 		return "member/memberPage";
 		
 	}
+	
+	@RequestMapping("/ajaxMemPageUserList.do")
+	@ResponseBody
+	public HashMap<String, List> ajaxMemPageUserList(WorkspaceJoinVO wJvo){
+		HashMap<String, List> map = new HashMap<String, List>();
+		map.put("userList", workspaceJoinDao.workspaceTotalMember(wJvo));
+		map.put("userAuthor", memWorkspaceJoinDao.workspaceJoinIdselect(wJvo));
+		
+		return map;
+	}
+	
+	@RequestMapping("/ajaxBoardsNub.do")
+	@ResponseBody
+	public List<BoardVO> ajaxBoardsNub(BoardVO vo) {
+		String id = vo.getId();
+
+		return boardDao.boardList(id);
+	}
+	
+	@RequestMapping("/ajaxMemBoardsListRow.do")
+	@ResponseBody
+	public List<BoardVO> ajaxMemBoardsListRow(BoardVO vo){
+		String id = vo.getId();
+		
+		return boardDao.boardList(id);
+				
+	}
+
+	@RequestMapping("/ajaxMemChangePermission.do")
+	@ResponseBody
+	public WorkspaceJoinVO ajaxMemChangePermission(WorkspaceJoinVO vo){
+		
+		return memWorkspaceJoinDao.workspaceJoinUserIdselect(vo);
+		
+	}
+	
 	
 	
 }
